2689a9d329c9a2770224feecad9e9a218979963b
AbdelmounaimSabriCTK <abdelmounaim.sabri@consitek.com>
2026-03-06 09:56:53 +0100

    Production sync

--- content/keyboard/shortcuts.js
+++ content/keyboard/shortcuts.js
@@ -6,11 +6,6 @@ export const setupKeyboardShortcuts = () => {
 
     const isNetSuiteByUrl = location.hostname.includes("netsuite.com");
     if (!isNetSuiteByUrl) return;
-
-    // Check if both "c" and "s" are pressed
-    if (keysPressed["c"] && keysPressed["s"]) {
-      chrome.runtime.sendMessage({ type: "OPEN_MAIN_SETUP" });
-    }
   });
 
   document.addEventListener("keyup", (e) => {
---
+++ dist/vue-ui/assets/typescript-61QQQ8EF.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./typescript-Drnc7qBf.js";export{t as conf,e as language};
